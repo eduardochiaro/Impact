03369adbf98519a11231228335a490b0a8422ff5
Eduardo Chiaro <e.chiaro@gmail.com>
2021-07-05 22:09:20 -0700

    Fix code lines plugin

--- assets/built/main.js
+++ assets/built/main.js
@@ -700,8 +700,8 @@ const useLazy = false;
 ;(function( $ ){
   hljs.highlightAll();
   hljs.addPlugin({
-    'after:highlightElement': ({ el, result }) => {
-      $("pre code.hljs").html(function(index, html) {
+    'after:highlightElement': ({el, result}) => {
+      $(el).html(function(index, html) {
         return html.trim().replace(/^(.*)$/mg, "<span class=\"line\">$1</span>");
       });
     }
--- assets/js/4_code.js
+++ assets/js/4_code.js
@@ -1,8 +1,8 @@
 ;(function( $ ){
   hljs.highlightAll();
   hljs.addPlugin({
-    'after:highlightElement': ({ el, result }) => {
-      $("pre code.hljs").html(function(index, html) {
+    'after:highlightElement': ({el, result}) => {
+      $(el).html(function(index, html) {
         return html.trim().replace(/^(.*)$/mg, "<span class=\"line\">$1</span>");
       });
     }
